2a795add23fd7587f90e897e7559cb981c1b1878
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-03-07 19:42:53 -0300

    feat: add new files and configurations

---
+++ modules/landing-page/screens/Home/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import Header from '@/modules/landing-page/components/Header/Header.vue';
+import Hero from '@/modules/landing-page/components/Hero/Hero.vue';
+</script>
+
+<template>
+  <div>
+    <Header />
+    <Hero />
+  </div>
+</template>
