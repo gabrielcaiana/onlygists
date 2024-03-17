7111629b53866d94cf35e8f16784f60dd62a1e3a
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-03-17 11:59:07 -0300

    feat: add DialogPaymentSuccess component to MoreDetail.vue

--- modules/gists/screens/MoreDetail/MoreDetail.vue
+++ modules/gists/screens/MoreDetail/MoreDetail.vue
@@ -1,11 +1,25 @@
 <script setup lang="ts">
 import PublicHeadline from '@/modules/gists/components/PublicHeadline/PublicHeadline.vue'
 import GistCodeSpippet from '@/modules/gists/components/GistCodeSpippet/GistCodeSpippet.vue'
+import DialogPaymentSuccess from '@/modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue';
+
+const route = useRoute()
+const isPaymentSuccessfully:Ref<boolean> = ref(false)
+
+onMounted(() => {
+  const { success_payment } = route.query
+
+  if(success_payment) {
+    isPaymentSuccessfully.value = true
+  }
+})
+
 </script>
 
 <template>
   <div>
     <PublicHeadline />
     <GistCodeSpippet />
+    <DialogPaymentSuccess v-model:visible="isPaymentSuccessfully" />
   </div>
 </template>
---
+++ modules/payments/components/DialogPaymentSuccess/DialogPaymentSuccess.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const visible = defineModel<boolean>('visible')
+
+console.log(visible.value)
+</script>
+
+<template>
+  <Dialog v-model:visible="visible" modal header="Compra efetuada com sucesso!" :style="{ width: '400px' }">
+    <div class="m-0 flex flex-col items-center justify-center w-full">
+      <i class="pi pi-check-circle text-orange-500 text-6xl my-5" />
+      <p class="font-[Inter] text-gray-700 text-center">
+        Parabéns pela compra! <br />
+        Você receberá no e-mail fornecido no checkout o seu gist 🤩
+      </p>
+    </div>
+
+    <template #footer>
+      <div class="flex justify-center w-full">
+        <Button label="Voltar" icon="pi pi-arrow-left" autofocus @click="visible = false" />
+      </div>
+    </template>
+  </Dialog>
+</template>
