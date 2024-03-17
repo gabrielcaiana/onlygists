fcd275d9275de7da2e422ef27909400c59371980
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-03-17 13:16:28 -0300

    chore: add profiles table to the database

---
+++ supabase/migrations/20240317153812_create_profiles_table.sql
@@ -0,0 +1,13 @@
+create table
+  profiles (
+    id uuid primary key references auth.users(id),
+    email varchar not null,
+    name varchar not null,
+    site varchar,
+    phone varchar,
+    bio varchar,
+    avatar_url varchar,
+    address jsonb,
+    created_at timestamp with time zone default current_timestamp not null,
+    payment_connected_account varchar
+  )
