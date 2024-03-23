5db0e56d88e83a3011aac685e97d622f4d21c992
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-03-23 14:52:57 -0300

    feat: create github redirect page

---
+++ modules/auth/screens/OAuthRedirect/OAuthRedirect.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+const router = useRouter()
+const navigateToPanel = async () => {
+  await wait(2000)
+  router.push('/app/panel')
+}
+
+onMounted(() => navigateToPanel())
+</script>
+
+<template>
+  <MainContent>
+    <template #content>
+      <p class="font-[Inter] text-gray-800 text-base 🤩">
+        🤩 Redirecionando...
+      </p>
+    </template>
+  </MainContent>
+</template>
