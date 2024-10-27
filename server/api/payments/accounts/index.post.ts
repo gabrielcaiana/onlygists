82bb8d9a168492375806a2746fbb0cf8c30d7471
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-10-27 13:12:55 -0300

    chore: create account stripe endpoint

--- server/api/payments/accounts/index.post.ts
+++ server/api/payments/accounts/index.post.ts
@@ -1,5 +1,6 @@
 import { useServerStripe } from "#stripe/server";
 import { serverSupabaseClient } from "#supabase/server";
+import { Database } from "@/libs/supabase/schema";
 
 interface RequestOptions {
   email: string
@@ -23,4 +24,32 @@ export default defineEventHandler(async (event) => {
       statusMessage: 'Unauthorized'
     })
   }
+
+  const config = useRuntimeConfig()
+  const stripe = await useServerStripe(event)
+  const supabase = await serverSupabaseClient<Database>(event)
+
+  const account = await stripe.accounts.create({
+    type: 'express',
+    email: payment.email,
+    country: 'BR',
+    business_type: 'individual',
+  })
+
+  supabase.from('profiles').update({
+    payment_connected_account: account.id
+  }).eq('email', payment.email)
+
+  const accountLink = await stripe.accountLinks.create({
+    account: account.id,
+    refresh_url: `${config.public.siteUrl}/app/panel`,
+    return_url: `${config.public.siteUrl}/app/panel`,
+    type: 'account_onboarding'
+  })
+
+  return {
+    accountId: account.id,
+    onboardingUrl: accountLink.url
+  }
 })
+
