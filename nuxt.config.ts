32ad70ea56fd4d4eeb65312b7e434f3a57409f4a
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-10-28 08:01:57 -0300

    feat: create endpoint to checkout

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -71,6 +71,9 @@ export default defineNuxtConfig({
   },
 
   runtimeConfig: {
+    stripeProductId5BRL: process.env.STRIPE_PRODUCT_ID_5BRL,
+    stripeProductId10BRL: process.env.STRIPE_PRODUCT_ID_10BRL,
+    stripeProductId25BRL: process.env.STRIPE_PRODUCT_ID_25BRL,
     public: {
       nodeEnv: process.env.NODE_ENV,
       supabaseUrl: process.env.SUPABASE_URL,
